466e056c5005884b9529b16a0c6ad157894aeae4
Shekhar Kumar <kumar.shekhar@live.com>
2019-04-22 11:06:41 -0400

    First commit

---
+++ src/main/java/com/shekhar/login/SpringSecurityCustomLoginApplication.java
@@ -0,0 +1,13 @@
+package com.shekhar.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCustomLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCustomLoginApplication.class, args);
+	}
+
+}
